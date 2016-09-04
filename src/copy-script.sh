69d084a4c6b97afae08b272fad7128da0ea81058
zonk77 <zonk77@gmail.com>
2016-09-04 22:46:46 +0200

    Create copy-script.sh

---
+++ src/copy-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p /nobody/.config/ghb/Convert-Script /nobody/.config/ghb/Watch-Folder
+chown -R nobody:users /nobody/.config/ghb
+if [ -f "/nobody/.config/ghb/Convert-Script/convert.sh" ]; then
+echo "using existing script, may require editing"
+chown -R nobody:users /nobody/.config/ghb
+chmod +x /nobody/.config/ghb/Convert-Script/convert.sh
+else
+cp /convert.sh /nobody/.config/ghb/Convert-Script/convert.sh
+chmod +x /nobody/.config/ghb/Convert-Script/convert.sh
+chown -R nobody:users /nobody/.config/ghb
+fi
